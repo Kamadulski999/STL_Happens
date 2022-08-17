fb206a8e67fff7d45d57144e6954ae1d1a68439b
samsiebert <sammsiebert@gmail.com>
2022-08-17 15:04:32 -0500

    just making sure everything is there

--- public/javascript/make-post.js
+++ public/javascript/make-post.js
@@ -9,15 +9,15 @@ async function makePost(event) {
 
   async function newFormHandler(event) {
     event.preventDefault();
-  
+
     const title = document.querySelector('input[name="post-title"]').value;
-    const post_text = document.querySelector('input[name="post-text"]').value;
+    const post_content = document.querySelector('input[name="post-text"]').value;
   
     const response = await fetch(`/api/posts`, {
       method: 'POST',
       body: JSON.stringify({
         title,
-        post_text
+        post_content
       }),
       headers: {
         'Content-Type': 'application/json'
