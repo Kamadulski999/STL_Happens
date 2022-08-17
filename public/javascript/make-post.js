240f0414c96f2234946032804cdecf41a96cff39
samsiebert <sammsiebert@gmail.com>
2022-08-17 14:34:36 -0500

    styling pretty much done

--- public/javascript/make-post.js
+++ public/javascript/make-post.js
@@ -1,10 +1,34 @@
-async function newFormHandler(event) {
+async function makePost(event) {
     event.preventDefault();
     document.location.replace('/dashboard');
 
   }
+  
+  document.querySelector('#make-post').addEventListener('click', makePost);
+  
 
+  async function newFormHandler(event) {
+    event.preventDefault();
   
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post_text = document.querySelector('input[name="post-text"]').value;
   
-  document.querySelector('#make-post').addEventListener('click', newFormHandler);
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        post_text
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
   
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
