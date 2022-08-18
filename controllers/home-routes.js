fb42fb25daa0ab3dcf70acdd4ec5a381665d957f
carfiguru@hotmail.com <carfiguru@hotmail.com>
2022-08-17 20:00:28 -0500

    setup href lings to tag routes, debugged a couple of routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -89,6 +89,51 @@ router.get('/post/:id', (req, res) => {
     });
 });
 
+router.get('/tag/:tag', (req, res) => {
+  Post.findAll({
+    where: {
+      tag: req.params.tag
+    },
+    attributes: [
+      'id',
+      'post_content',
+      'title',
+      'created_at',
+      'tag'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+
+      res.render('homepage', {
+        posts,
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
--- models/Post.js
+++ models/Post.js
@@ -31,7 +31,7 @@ Post.init(
     },
     tag: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: true
     }
   },
   {
